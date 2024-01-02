8db8f147d384c7bd9d232f55034a32a4c04c6979
mbrezov <mariobrezovecki@gmail.com>
2024-01-02 11:43:12 +0100

    New DB models

--- server/models/categoryModel.js
+++ server/models/categoryModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+/* const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
@@ -12,6 +12,51 @@ const categorySchema = new Schema({
         required: false,
         default: "#a6a6a6",
     },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
+ */
+
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    buy: {
+        type: Boolean,
+    },
+});
+
+const subcategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    products: [productSchema],
+});
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    color: {
+        type: String,
+        required: false,
+        default: "#a6a6a6",
+    },
+    subcategory: [subcategorySchema],
 });
 
 module.exports = mongoose.model("Category", categorySchema);
