f51445c7adfcb3437820268edcc666a7004324b6
mbrezov <mariobrezovecki@gmail.com>
2024-02-11 13:15:22 +0100

    HomePage - cleaning code

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,5 @@
 import styles from "./HomePage.module.scss";
 import CategoryCard from "../../components/CategoryCard/CategoryCard";
-/* import axios from "axios";
-import { useEffect, useState } from "react"; */
 import { ICategory } from "../../interfaces/interface";
 
 interface IProps {
@@ -9,21 +7,6 @@ interface IProps {
 }
 
 const Homepage = ({ categories }: IProps) => {
-    /*     const [categories, setCategories] = useState([]);
-
-    useEffect(() => {
-        axios
-            .get("http://localhost:4000/api/category")
-            .then((res) => {
-                setCategories(res.data);
-            })
-            .catch((error) => {
-                console.error("Error fetching categories:", error);
-            });
-    }, []); */
-
-    /*    console.log(categories); */
-
     return (
         <div>
             <h1>Kategorije</h1>
