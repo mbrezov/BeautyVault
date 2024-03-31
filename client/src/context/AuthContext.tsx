ec454c43d47ad80c77be8e4e9833228d9adeb9a5
mbrezov <mariobrezovecki@gmail.com>
2024-03-31 22:38:53 +0200

    AuthContext added

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useReducer } from "react";
+
+export const AuthContext = createContext<any>(null);
+
+export const authReducer = (state: any, action: any) => {
+    switch (action.type) {
+        case "LOGIN":
+            return {
+                user: action.payload,
+            };
+        case "LOGOUT":
+            return {
+                user: null,
+            };
+        default:
+            return state;
+    }
+};
+
+export const AuthContextProvider = ({ children }: any) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null,
+    });
+
+    console.log("AuthContesxt state: ", state);
+
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
