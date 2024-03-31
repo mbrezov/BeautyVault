3829c64c05c551acc21718b1c6df73df024a0baf
mbrezov <mariobrezovecki@gmail.com>
2024-03-31 22:14:40 +0200

    Login feature implemented

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,11 +16,25 @@ const userSchema = new mongoose.Schema({
 
 //static signup method
 userSchema.statics.signup = async function (email, password) {
+    //validation
+    if (!email || !password) {
+        throw Error("All fileds must be filled");
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error("Email is not valid");
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password is not string enough");
+    }
+
     const exists = await this.findOne({ email });
 
     if (exists) {
         throw Error(`Email ${email} already exists`);
     }
+
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt); //password hash
 
@@ -27,6 +42,27 @@ userSchema.statics.signup = async function (email, password) {
         email,
         password: hash,
     });
+
+    return user;
+};
+
+//static login method
+userSchema.statics.login = async function (email, password) {
+    if (!email || !password) {
+        throw Error("All fileds must be filled");
+    }
+
+    const user = await this.findOne({ email });
+
+    if (!user) {
+        throw Error(`Incorrect E-mail`);
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+        throw Error("Incorrect password");
+    }
 
     return user;
 };
